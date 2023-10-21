0d153be04d0327743db25089cba04919db94a205
OdericoOfficial <2919128969@qq.com>
2023-10-21 22:00:04 +0800

    add MagicOnionModule and GrpcWebSocketBridgeModule for game server

---
+++ ModuleDistributor.Grpc/GrpcServiceModule.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Routing;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ModuleDistributor.Grpc
+{
+    [DependsOn(typeof(GrpcModule))]
+    public class GrpcServiceModule<TEntryModule> : CustomModule
+        where TEntryModule : CustomModule
+    {
+        public override void OnApplicationInitialization(ApplicationContext context)
+        {
+            Assembly assembly = typeof(TEntryModule).Assembly;
+            List<Expression> list = new List<Expression>();
+            List<ParameterExpression> variables = new List<ParameterExpression>();
+            var param1 = Expression.Parameter(typeof(GrpcEndpointRouteBuilderExtensions));
+            var param2 = Expression.Parameter(typeof(IEndpointRouteBuilder));
+
+            foreach (var item in assembly.GetTypes())
+                if (item.GetCustomAttribute<GrpcServiceAttribute>() is null)
+                    list.Add(Expression.Call(param1, "MapGrpcService", new Type[] { item }, param2));
+
+            Expression.Lambda<Action<IEndpointRouteBuilder>>(Expression.Block(list), param2)
+                .Compile()
+                .Invoke(context.EndPoint);
+        }
+    }
+}
---
+++ ModuleDistributor.MagicOnion/MagicOnionModule.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using ModuleDistributor.Grpc;
+
+namespace ModuleDistributor.MagicOnion
+{
+    [DependsOn(typeof(GrpcModule))]
+    public class MagicOnionModule : CustomModule
+    {
+        public override void ConfigureServices(ServiceContext context)
+            => context.Services.AddMagicOnion();
+
+        public override void OnApplicationInitialization(ApplicationContext context)
+            => context.EndPoint.MapMagicOnionService();
+    }
+}
